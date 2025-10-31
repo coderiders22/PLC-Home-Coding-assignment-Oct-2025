77f7e68201056e0ae4cc7c3eb38674582d38a7fd
coderiders22 <manavrai454@gmail.com>
2025-10-31 15:45:44 +0530

    updated

--- mini-project-manager/frontend/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -eu
-
-# Substitute $PORT into nginx conf template
-envsubst '\$PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-# Start nginx
-exec nginx -g 'daemon off;'
